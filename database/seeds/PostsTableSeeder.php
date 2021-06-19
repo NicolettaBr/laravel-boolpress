1922c1b965902d9be3a6e4b3de574b19dd7796b1
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-06-19 11:01:24 +0200

    creata migration, model e seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++){
+
+            $new_post = new Post();
+            $new_post->title = $faker->sentence(3);
+            $new_post->content = $faker->paragraphs(4, true);
+            $new_post->slug = Str::slug($new_post->title, '-');
+            $new_post->save();
+        }
+    }
+}
